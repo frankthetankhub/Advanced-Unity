09c46542f7c1b5a11cd3793b1c5d355e776be406
frankthetankhub <61543714+frankthetankhub@users.noreply.github.com>
2021-09-26 16:39:16 +0200

    Add files via upload

---
+++ InnerDeamonFinal/Assets/PlayerUnit.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUnit : Unit
+{
+    public float cooldownToEndTurn;
+    public bool canEndTurn;
+    public void Awake()
+    {
+        base.Awake();
+    }
+    public void Start()
+    {
+        base.Start();
+        cooldownToEndTurn = 0;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        base.Update();
+        if (turn)
+        {
+            EndTurnCooldownHandler();
+        }
+        // refactor to onvaluechanged
+        targetGameObject = GameManager.instance.selectedObject;
+        //----
+        if (Input.GetKeyUp("a") & turn )
+        {
+            base.BasicAttack();
+        }
+        
+        if (Input.GetKeyDown("e") & !ismoving & turn & canEndTurn)
+        {
+            canEndTurn = false;
+            print(name +"ended my turn");
+            GameManager.instance.RoundOver();
+        }
+    }
+    public void EndTurnCooldownHandler()
+    {
+        if (!canEndTurn)
+        {
+            cooldownToEndTurn += Time.deltaTime;
+        }
+        if (cooldownToEndTurn >= 1)
+        {
+            canEndTurn = true;
+            cooldownToEndTurn = 0;
+        }
+    }
+    
+}
